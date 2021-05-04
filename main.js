afaeb9796d9118469557f8403b78d2bb0ff97189
celestebohan <bohac717@newschool.edu>
2021-05-04 18:50:45 -0400

    add countries

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ var canada = document.getElementById("canada");
 var mexico = document.getElementById("mexico");
 var china = document.getElementById("china");
 var southafrica = document.getElementById("southafrica");
-// var algeria = document.getElementById("algeria");
-// var thailand = document.getElementById("thailand");
+var algeria = document.getElementById("algeria");
+var thailand = document.getElementById("thailand");
+var australia = document.getElementById("australia");
+var greece = document.getElementById("greece");
+var netherlands = document.getElementById("netherlands");
+var russia = document.getElementById("russia");
 
-var imgArray = [us, canada, mexico, china, southafrica];
+var imgArray = [us, canada, mexico, china, southafrica, algeria, thailand, australia, greece, netherlands, russia];
 function showImg(img) {
   for (i = 0; i < imgArray.length; i++) {
     imgArray[i].style.display = "none";
@@ -69,7 +73,7 @@ china.addEventListener('animationend', () => {
 }, { once: true });
 
     china.scrollIntoView({behavior: "smooth"});
-  };if (pos.coords.longitude > 76 && pos.coords.longitude < 121 && pos.coords.latitude <42 && pos.coords.latitude > 22){
+  };if (pos.coords.longitude > 17 && pos.coords.longitude < 32 && pos.coords.latitude <-25 && pos.coords.latitude > -34){
     showImg(southafrica);
 
         southafrica.classList.add('fade-in');
@@ -78,7 +82,60 @@ southafrica.addEventListener('animationend', () => {
 }, { once: true });
 
     southafrica.scrollIntoView({behavior: "smooth"});
-  };
+  };if (pos.coords.longitude > -8 && pos.coords.longitude < 12 && pos.coords.latitude <36 && pos.coords.latitude > 19){
+    showImg(algeria);
+
+        algeria.classList.add('fade-in');
+algeria.addEventListener('animationend', () => {
+    algeria.classList.remove('fade-in');
+}, { once: true });
+
+    algeria.scrollIntoView({behavior: "smooth"});
+ };if (pos.coords.longitude > 97 && pos.coords.longitude < 105 && pos.coords.latitude <20 && pos.coords.latitude > 12){
+    showImg(thailand);
+
+        thailand.classList.add('fade-in');
+thailand.addEventListener('animationend', () => {
+    thailand.classList.remove('fade-in');
+}, { once: true });
+
+    thailand.scrollIntoView({behavior: "smooth"});
+ };if (pos.coords.longitude > 144 && pos.coords.longitude < 153 && pos.coords.latitude <-11 && pos.coords.latitude > -38){
+    showImg(australia);
+
+        australia.classList.add('fade-in');
+australia.addEventListener('animationend', () => {
+    australia.classList.remove('fade-in');
+}, { once: true });
+
+    australia.scrollIntoView({behavior: "smooth"});
+ };if (pos.coords.longitude > 19 && pos.coords.longitude < 24 && pos.coords.latitude <40 && pos.coords.latitude > 36){
+    showImg(greece);
+
+        greece.classList.add('fade-in');
+greece.addEventListener('animationend', () => {
+    greece.classList.remove('fade-in');
+}, { once: true });
+
+    greece.scrollIntoView({behavior: "smooth"});
+ };if (pos.coords.longitude > 3 && pos.coords.longitude < 7 && pos.coords.latitude <53 && pos.coords.latitude > 51){
+    showImg(netherlands);
+
+        netherlands.classList.add('fade-in');
+netherlands.addEventListener('animationend', () => {
+    netherlands.classList.remove('fade-in');
+}, { once: true });
+
+    netherlands.scrollIntoView({behavior: "smooth"});
+ };if (pos.coords.longitude > 28 && pos.coords.longitude < -171 && pos.coords.latitude <76 && pos.coords.latitude > 87){
+    showImg(russia);
+
+        russia.classList.add('fade-in');
+russia.addEventListener('animationend', () => {
+    russia.classList.remove('fade-in');
+}, { once: true });
+
+    russia.scrollIntoView({behavior: "smooth"});
  };
 function showError(error) {
     bin.innerHTML = error.message;
